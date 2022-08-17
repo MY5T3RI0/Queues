13109c3462fee1b2ae6b3392b94b4c13e5352a15
MY5T3RI0 <66646109+MY5T3RI0@users.noreply.github.com>
2022-08-17 20:18:41 +0300

    Добавьте файлы проекта.

---
+++ Queues/Model/SimpleQueue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues.Model
+{
+    class SimpleQueue<T>
+    {
+        private List<T> Items = new List<T>();
+        private T Tail => Items.First();
+        private int Count => Items.Count();
+
+        public SimpleQueue()
+        {
+
+        }
+
+        public SimpleQueue(T data)
+        {
+            Items.Add(data);
+        }
+
+        public void Enqueque(T data)
+        {
+            Items.Add(data);
+        }
+
+        public T Dequeue()
+        {
+            if (Count == 0)
+            {
+                throw new NullReferenceException("Очередь пуста");
+            }
+            var item = Tail;
+            Items.Remove(item);
+            return item;
+        }
+
+        public T Peek()
+        {
+            if (Count == 0)
+            {
+                throw new NullReferenceException("Очередь пуста");
+            }
+            return Tail;
+        }
+    }
+}
---
+++ Queues/Program.cs
@@ -0,0 +1,33 @@
+﻿using Queues.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var SimpleQueue = new SimpleQueue<int>();
+
+            SimpleQueue.Enqueque(1);
+            SimpleQueue.Enqueque(2);
+            SimpleQueue.Enqueque(3);
+            SimpleQueue.Enqueque(4);
+            SimpleQueue.Enqueque(5);
+
+            Console.WriteLine(SimpleQueue.Dequeue());
+            Console.WriteLine(SimpleQueue.Dequeue());
+            Console.WriteLine(SimpleQueue.Dequeue());
+            Console.WriteLine(SimpleQueue.Dequeue());
+            Console.WriteLine(SimpleQueue.Peek());
+            Console.WriteLine(SimpleQueue.Dequeue());
+
+            Console.ReadLine();    
+        }
+    }
+}
